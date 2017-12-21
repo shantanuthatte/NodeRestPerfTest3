7735de58f112f0d5243f0a17cc044450e373ad08
Shantanu Thatte <shantanuthatte@gmail.com>
2017-12-21 11:02:20 -0500

    Update install-alpine.sh

--- install-alpine.sh
+++ install-alpine.sh
@@ -11,6 +11,9 @@ npm install adonis-cli -g
 adonis new yardstick
 cp yardstick-changes/app/Http/kernel.js yardstick/app/Http/kernel.js
 cp yardstick-changes/app/Http/routes.js yardstick/app/Http/routes.js
+cd yardstick
+npm install
+cd ..
 npm install sails -g
 sails new test-project
 cp test-project-changes/api/controllers/ApiController.js test-project/api/controllers/ApiController.js
